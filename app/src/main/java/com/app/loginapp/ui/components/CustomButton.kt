198c169b3e8c41e5f7a75f30b3d9204164f61707
Aslı <aslibyr00@gmail.com>
2024-08-19 15:25:18 +0300

    First commit

---
+++ app/src/main/java/com/app/loginapp/ui/components/CustomButton.kt
@@ -0,0 +1,10 @@
+package com.app.loginapp.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomButton(modifier: Modifier = Modifier) {
+
+
+}
---
+++ app/src/main/java/com/app/loginapp/ui/theme/Theme.kt
@@ -0,0 +1,22 @@
+package com.app.loginapp.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val AppColors = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+@Composable
+fun LoginAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = AppColors,
+        typography = Typography,
+        content = content
+    )
+}
