eb1ee51bd3cd9c46300bd2c01b2c9288d31f1aba
Aslı <aslibyr00@gmail.com>
2024-08-20 18:27:39 +0300

    LoginScreen contents added.

--- app/src/main/java/com/app/loginapp/ui/ui/login/content/LoginEmailContent.kt
+++ app/src/main/java/com/app/loginapp/ui/ui/login/content/LoginEmailContent.kt
@@ -1,33 +1,39 @@
-package com.app.loginapp.ui.ui
+package com.app.loginapp.ui.ui.login.content
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
-import com.app.loginapp.ui.components.CustomButton
+import androidx.compose.ui.unit.dp
 import com.app.loginapp.ui.components.CustomTextField
 
+
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun LoginScreenEmailContent(
+    modifier: Modifier = Modifier,
+    email: String,
+    returnEmail: (String) -> Unit,
+) {
     Column(
-        modifier = modifier
+        modifier
             .fillMaxSize()
-            .systemBarsPadding()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        val text = remember { mutableStateOf("") }
 
         CustomTextField(
-            modifier = Modifier.fillMaxWidth(),
-            label = "Text",
-            text = text.value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            label = "Email",
+            text = email,
             returnText = { input ->
-                text.value = input
+                returnEmail.invoke(input)
             },
             onImeClicked = {},
             keyboardOptions = KeyboardOptions.Default.copy(
@@ -35,6 +41,5 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             ),
 //            errorMessage = if (text.value.isEmpty() && isFocused) "Bu alan boş bırakılamaz" else null
         )
-        CustomButton(onClick = {}, buttonText = "Buton")
     }
 }
